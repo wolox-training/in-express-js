b904374c2d1d876afe428434f0326a213477b199
Nacho Nieva <ignacio.nieva@wolox.com.ar>
2018-05-22 17:10:47 -0300

    user serialize model for user table in wolox_land db

---
+++ app/models/user-model.js
@@ -0,0 +1,38 @@
+const users = sequelize.define('users', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true
+  },
+  firstname: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastname: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  created_at: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW
+  },
+  updated_at: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW
+  },
+  deleted_at: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW
+  }
+});
